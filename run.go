d6221e73bc8a64be47542afcd205b4ebf6dabb4e
c-bata <contact@c-bata.link>
2016-04-01 02:31:47 +0900

    Add web page

--- run.go
+++ run.go
@@ -5,6 +5,9 @@ import (
 	"github.com/c-bata/gosearch/crawler"
 	"github.com/c-bata/gosearch/env"
 	"github.com/c-bata/gosearch/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/c-bata/gosearch/search"
 )
 
 func main() {
@@ -15,9 +18,22 @@ func main() {
 		fmt.Println("Cannot connect to MongoDB")
 		return
 	}
-	go crawler.Crawl(seed, 4, msg)
 
-	for m := range msg {
-		fmt.Println(m)
-	}
+	go func() {
+		go crawler.Crawl(seed, 4, msg)
+
+		for m := range msg {
+			fmt.Println(m)
+		}
+	} ()
+
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		keyword := c.Query("keyword")
+		urls := search.Search(keyword)
+		c.JSON(http.StatusOK, gin.H{
+			"results": urls,
+		})
+	})
+	router.Run(":8080")
 }
